8b4512797a6ee1d5e867d396d0cdf44f8e6af546
Gabs Ghidini <43341520+gabsghidini@users.noreply.github.com>
2022-05-05 20:27:24 -0300

    fixed a few typos and added the test

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// 2 loops , i e j, i vai ser o primeiro nível, j segundo nivel
-// const arrTeste = [[0, 0, 0, 0, 0, 0], [0, 1, 2, 3, 4, 5], [0, 2, 4, 6, 8, 10], [0, 3, 6, 9, 12, 15], [0, 4, 8, 12, 16, 20] , [0, 5, 10, 15, 20, 25]]
-
 function multiplicacaoDe10 () {
   let resultado = [];
   let resultadoAux = [];
@@ -30,5 +27,9 @@ function tabelaMultiplicacao (num) {
   return resultado;
 }
 
-// console.table(tabelaMultiplicacao(5));
-// console.table(multiplicacaoDe10());
+/*
+
+console.table(tabelaMultiplicacao(5)); <- Usado no teste de mesa
+console.table(multiplicacaoDe10()); <- Usado no teste de mesa
+
+*/ 
